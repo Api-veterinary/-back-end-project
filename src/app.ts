0ebc450d4666302415a444e4bea0476aa73daff1
Germana-1 <germanagerriane@yahoo.com.br>
2023-01-10 14:19:56 -0300

    feat:POST Doctor

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import "express-async-errors";
 import express from "express";
+import doctorsRoutes from "./routers/doctors.routes";
 
 const app = express();
 app.use(express.json());
+
+app.use("/doctors", doctorsRoutes);
 
 export default app;
