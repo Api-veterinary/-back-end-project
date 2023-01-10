22aba0d2b3dd6c5d488a9f32dd74db65d6fdef87
Germana-1 <germanagerriane@yahoo.com.br>
2023-01-10 15:16:41 -0300

    fix: doctor schema

---
+++ src/schemas/doctors.schemas.ts
@@ -0,0 +1,37 @@
+import * as yup from "yup";
+
+export const doctorSchema = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+  crmmv: yup.number().notRequired(),
+  address: yup
+    .object()
+    .required()
+    .shape({
+      district: yup.string(),
+      zipCode: yup.string().max(8),
+      number: yup.string().notRequired(),
+      city: yup.string(),
+      state: yup.string().max(2),
+    }),
+});
+
+export const doctorWithoutPasswordSchema = yup.object().shape({
+  id: yup.string().notRequired(),
+  name: yup.string().notRequired(),
+  email: yup.string().email().notRequired(),
+  crmmv: yup.number().notRequired(),
+  createdAt: yup.date().notRequired(),
+  updatedAt: yup.date().notRequired(),
+  address: yup
+    .object()
+    .notRequired()
+    .shape({
+      district: yup.string(),
+      zipCode: yup.string().max(8),
+      number: yup.string().notRequired(),
+      city: yup.string(),
+      state: yup.string().max(2),
+    }),
+});
