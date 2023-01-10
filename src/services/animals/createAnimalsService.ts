62766c13dae8097ed868766e840302342ef12a92
Erik <eriksn07@gmail.com>
2023-01-10 14:38:05 -0300

    Feat: Schema animals

--- src/services/animals/createAnimalsService.ts
+++ src/services/animals/createAnimalsService.ts
@@ -1,12 +1,19 @@
 import AppDataSource from "../../data-source";
 import { Animals } from "../../entities/animals/animals.entity";
+import { animalsWithoutPasswordSchema } from "../../schemas/animalsSchema";
 
-export const createAnimalsService = async (data): Promise<Animals[]> => {
+export const createAnimalsService = async (data) => {
   const animalsRepository = AppDataSource.getRepository(Animals);
 
   const newAnimals = animalsRepository.create(data);
 
   await animalsRepository.save(newAnimals);
 
-  return newAnimals;
+  const animalsWithoutPassord = await animalsWithoutPasswordSchema.validate(
+    newAnimals,
+    {
+      stripUnknown: true,
+    }
+  );
+  return animalsWithoutPassord;
 };
