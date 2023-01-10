308d5df5397cded3128a6082bf3bb73a83b2cfec
Henrique Carderan <rike.c.12@gmail.com>
2023-01-10 11:42:31 -0300

    feature: emailMiddleware

---
+++ src/middlewares/ensureEmailAvailability.middleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import "dotenv/config";
+import AppDataSource from "../data-source";
+import { Doctors } from "../entities/doctors/doctors.entity";
+
+const ensureEmailAvailabilityMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email } = req.body;
+
+  const userRepository = AppDataSource.getRepository(Doctors);
+
+  const userAlreadyExists = await userRepository.findOneBy({
+    email: email,
+  });
+
+  if (userAlreadyExists) {
+    return res
+      .status(400)
+      .json({ message: "This email adress is already being used" });
+  }
+  next();
+};
+
+export default ensureEmailAvailabilityMiddleware;
