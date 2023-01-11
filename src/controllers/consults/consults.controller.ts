48305499500ed2e3f660ca69b27f364490e5feb2
Germana-1 <germanagerriane@yahoo.com.br>
2023-01-11 16:08:15 -0300

    feat: delete route

--- src/controllers/consults/consults.controller.ts
+++ src/controllers/consults/consults.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import createConsultsService from "../../services/consults/createConsults.service";
+import deleteConsultsService from "../../services/consults/deleteConsults.service";
 
 const createConsultsController = async (
   request: Request,
@@ -10,4 +11,13 @@ const createConsultsController = async (
   return response.status(201).json(newConsults);
 };
 
-export { createConsultsController };
+const deleteConsultsController = async (
+  request: Request,
+  response: Response
+) => {
+  const id: string = request.params.id;
+  await deleteConsultsService(id);
+  return response.status(204).json({});
+};
+
+export { createConsultsController, deleteConsultsController };
--- src/routers/consults.routes.ts
+++ src/routers/consults.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { createConsultsController } from "../controllers/consults/consults.controller";
+import {
+  createConsultsController,
+  deleteConsultsController,
+} from "../controllers/consults/consults.controller";
 
 const consultsRoutes = Router();
 
 //precisa do login, para confirmar a autenticação pelo token
 consultsRoutes.post("", createConsultsController);
+
+//O :id é o id da consulta
+consultsRoutes.delete("/:id", deleteConsultsController);
 
 export default consultsRoutes;
